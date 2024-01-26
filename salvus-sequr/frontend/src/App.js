ce3cdf6ac9e973f6b0bd78607b3216a5a7dffeba
tarungolait <121688989+tarungolait@users.noreply.github.com>
2024-01-26 21:25:10 +0530

    error solved

--- salvus-sequr/frontend/src/App.js
+++ salvus-sequr/frontend/src/App.js
@@ -1,10 +1,9 @@
-// App.js
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import CombinedDataPage from './CombinedDataPage';
 import HomePage from './HomePage';
 import DataEntryForm from './DataEntryForm';
-import ProductTypesPage from './ProductTypesPage'; // Import the new component
+import ProductTypesPage from './ProductTypesPage';
 import LoginPage from './LoginPage';
 import './App.css';
 
@@ -27,9 +26,9 @@ const App = () => {
             path="/"
             element={loggedIn ? <HomePageWithHeader onLogout={handleLogout} /> : <LoginPage onLogin={handleLogin} />}
           />
-          <Route path="/combined-data" element={loggedIn ? <CombinedDataPage /> : <Navigate to="/" />} />
+          {/* Update the parent <Route> path to include a trailing "*" */}
+          <Route path="/combined-data/*" element={loggedIn ? <CombinedDataPage /> : <Navigate to="/" />} />
           <Route path="/data-entry" element={loggedIn ? <DataEntryForm /> : <Navigate to="/" />} />
-          {/* Add the route for the Product Types page */}
           <Route path="/product-types" element={loggedIn ? <ProductTypesPage /> : <Navigate to="/" />} />
         </Routes>
       </div>
@@ -44,7 +43,6 @@ const HomePageWithHeader = ({ onLogout }) => {
         <div className="header-content">
           <h1>Infinicue Solutions Data</h1>
           <nav>
-          
             <button onClick={onLogout}>Logout</button>
           </nav>
         </div>
